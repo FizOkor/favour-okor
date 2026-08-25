50b2041207344bfec5d47f4019f1ec2030ac3f5e
Favour Okor <okorfavour2000@gmail.com>
2026-08-25 16:32:27 +0100

    feat: changed font to host-grotesk

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 // import '@fontsource-variable/roboto-condensed'
-import '@fontsource/ubuntu-mono';
+// import '@fontsource/ubuntu-mono';
+// Supports weights 300-800
+import '@fontsource-variable/host-grotesk/wght.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
